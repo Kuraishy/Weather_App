0e83b308f074125eec2db9576fe4a530dd37b39e
Ricardo <legoricardo@hotmail.com>
2021-05-19 03:06:14 -0500

    color categories added to backend instead of front end

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -10,12 +10,39 @@ def home(request):
 
 	try:
 		api = json.loads(api_request.content)
+		categoryDescription, categoryColor = weatherStatus(api)
+
 
 	except Exception as e:
 		print(e)
 		api = e
 
-	return render(request, 'home.html',{'api':api}) 
+
+	return render(request, 'home.html',{'api':api, 'category_description':categoryDescription, 'category_color':categoryColor}) 
 
 def about(request):
 	return render(request, 'about.html',{}) 
+
+
+def weatherStatus(api):
+	categoria = api[0]['Category']['Name']
+	categoriaColor=''
+
+	if categoria == "Good":
+		return f"(0-50) Air quality is considered satisfactory, and air pollution poses litthle or no risk", "good"
+
+	elif categoria == "Moderate":
+		return f"(51-100) Air quality is considered aceptable", "moderate"
+
+	elif categoria == "Unhealthy for Sensitive Groups":
+		return f"(101-150) Air quality is considered unhealthy for people with some ilnesses", "usg"
+
+	elif categoria == "Unhealthy":
+		return "(151-200) Air quality is considered unhealthy", "unhealthy"
+
+	elif categoria == "Very Unhealthy":
+		return "(201-300) Air quality is considered unhealthy", "veryunhealthy"
+
+	elif categoria == "Hazardous":
+		return "(301-500) Air quality is considered hell ","hazardous"
+
